c471e478829593ce28859ba573d3b11a34ee2a19
aya-monem <aya.monem.14@gmail.com>
2020-03-20 14:50:11 +0200

    add (font-italic) to hotel3

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      autoplay:true,
+      autoplayHoverPause:true,
+      items:8,
+      nav:true,
+      dots:true,
+      loop:true,
+    });
+  });
